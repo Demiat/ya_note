8bc9c2982a4421a6cff832ed55787799ca70a6d5
Дмитрий Тарасов <demiat@mail.ru>
2025-01-07 23:10:20 +0300

    content

---
+++ notes/tests/test_content.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from notes.forms import NoteForm
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestContent(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Создадим форму
+        cls.form = NoteForm(data={'title': 'Это тест', 'text': 'test'})
+        cls.form2 = NoteForm(data={
+            'title': 'Это тест',
+            'text': 'test',
+            'slug': 'Проверим большой слаг 123' * 200
+        })
+        # Создадим заметки для автора и для других пользователей
+        cls.author = User.objects.create(username='Lion-T')
+        cls.another_user = User.objects.create(username='Another-Lion')
+        for user in (cls.author, cls.another_user):
+            Note.objects.bulk_create(
+                Note(
+                    title=f'Заметка {index} of {user}',
+                    text='Текст заметки',
+                    author=user,
+                    slug=f'slug-{index}-of-{user}'
+                ) for index in range(5)
+            )
+
+    def test_form(self):
+        """Проверяет поля формы"""
+        self.assertEqual(self.form.is_valid(), True)
+        # Форма с таким слагом не должна пройти
+        self.assertEqual(self.form2.is_valid(), False)
+
+        self.assertEqual(self.form.cleaned_data['title'], 'Это тест')
+        self.assertEqual(self.form.cleaned_data['text'], 'test')
+        self.assertEqual(self.form.cleaned_data['slug'], 'eto-test')
+
+    def test_author_list_notes(self):
+        """Проверяет, что автор может видеть только свои заметки в списке"""
+        self.client.force_login(self.author)
+        response = self.client.get(reverse('notes:list'))
+        object_list = response.context['object_list']
+        authors = [note.author for note in object_list]
+        for author in authors:
+            self.assertEqual(author, self.author)
--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -20,43 +20,45 @@ class TestRoutes(TestCase):
             slug='zag-test',
             author=cls.author)
         cls.another_user = User.objects.create(username='Другой Толстой')
+        cls.args = ('zag-test',)
+        cls.urls = (
+            ('notes:home', None),
+            ('notes:list', None),
+            ('notes:success', None),
+            ('notes:add', None),
+            ('notes:detail', cls.args),
+            ('notes:edit', cls.args),
+            ('notes:delete', cls.args),
+        )
 
     def test_pages_availability_for_anonymous(self):
-        urls = (
-            ('notes:home', None, HTTPStatus.OK),
-            ('notes:list', None, HTTPStatus.FOUND),
-            ('notes:add', None, HTTPStatus.FOUND),
-            ('notes:success', None, HTTPStatus.FOUND),
-        )
-        for reverse_name, args, status in urls:
+        """Тестирует доступ анонима к страницам"""
+        for reverse_name, args in self.urls[:1]:
             with self.subTest(name=reverse_name):
                 url = reverse(reverse_name, args=args)
                 response = self.client.get(url)
-                self.assertEqual(response.status_code, status)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_redirect_for_anonymous_client(self):
+        """Тестирует редирект анонима на страницу авторизации"""
         login_url = reverse('users:login')
-        for reverse_name in (
-            'notes:detail',
-            'notes:edit',
-            'notes:delete',
-        ):
+        for reverse_name, args in self.urls[1:]:
             with self.subTest(name=reverse_name):
-                url = reverse(reverse_name, args=('zag-test',))
+                url = reverse(reverse_name, args=args)
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
 
-    
     def test_availability_for_note_edit_and_delete(self):
+        """Тестирует доступ пользователей к чужим ресурсам"""
         users_statuses = (
             (self.author, HTTPStatus.OK),
             (self.another_user, HTTPStatus.NOT_FOUND),
         )
         for user, status in users_statuses:
             self.client.force_login(user)
-            for reverse_name in ('notes:edit', 'notes:delete'):  
-                with self.subTest(user=user, name=reverse_name):        
-                    url = reverse(reverse_name, args=('zag-test',))
+            for reverse_name, args in self.urls[5:]:
+                with self.subTest(user=user, name=reverse_name):
+                    url = reverse(reverse_name, args=args)
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
